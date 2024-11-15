00f7361c2958358afbdbfbdbb32a8b9d97af4ec8
PartyPieter <manusvanderham@outlook.com>
2024-11-15 16:10:28 +0100

    Added the approx function

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -9,7 +9,6 @@ def cmd_group():
     pass
 
 @cmd_group.command()
-#@click.argument("smallangle")
 @click.option(    
     "--number",
     "-n",
@@ -26,7 +25,6 @@ def sin(number):
     print(df)
 
 @cmd_group.command()
-#@click.argument("smallangle")
 @click.option(
     "--number",
     "-n",
@@ -42,9 +40,20 @@ def tan(number):
     df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
     print(df)
 
+@cmd_group.command()
+@click.argument("value" , type = float)
+def approx(value):
+    """Calculates the biggest angle for which the small angle approximation is still valid in radians
 
+    Args:
+    value (float): The value for which the biggest angle that upholds the small angle approximation is given
+    """    
+    x = 0.000
+    while np.abs(x - np.sin(x)) < value:
+        x = x + 0.001
 
-
+    x = x - 0.001    
+    print(f"Biggest angle for which the small angle approx is given: {x} rads")
 
 
 if __name__ == "__main__":
