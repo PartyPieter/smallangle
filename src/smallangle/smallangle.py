8cd2ae8a8406db86b27bf3d5a8cc9cdb69db73bf
PartyPieter <manusvanderham@outlook.com>
2024-11-15 14:01:53 +0100

    Added docstrings and sub commands sin,tan, number and starting comand smallangle

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -1,20 +1,53 @@
 import click
 import numpy as np
-from numpy import pi
 import pandas as pd
+from numpy import pi
 
 
+@click.group()
+def cmd_group():
+    pass
+
+
+@cmd_group.command()
+#@click.argument("smallangle")
+@click.option(
+    "--number",
+    "-n",
+    default = 10
+
+)
 def sin(number):
+    """Calculate the sin of NUMBER values between 0 and 2 pi
+
+    NUMBER is the amount values between 0 and 2 pi that the sin is calculated of
+"""  
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
     print(df)
 
+@cmd_group.command()
+#@click.argument("smallangle")
+@click.option(
+    "--number",
+    "-n",
+    default = 10
 
+)
 def tan(number):
+    """Calculate the tan of NUMBER values between 0 and 2 pi
+
+       NUMBER is the amount values between 0 and 2 pi that the tan is calculated of
+    """    
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
     print(df)
 
 
+
+
+
+
 if __name__ == "__main__":
-    sin(10)
+    cmd_group()
+
